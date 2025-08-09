8d73a22df66806ec3f323eb7005ed83e0bcd2b61
Yumesh Ban <yumeshban365@gmail.com>
2025-08-09 17:04:50 +0545

    Backend Core User Documentation

--- Futsal_backend/core/urls.py
+++ Futsal_backend/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import RegisterView, AdminCreateUserView, LoginView, LogoutView
-
+ # ----- Simple User Authentication and Login -----
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
